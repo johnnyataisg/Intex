23cac8a42d5421482d153db50fcd33c3beb9c72f
Johnny Pao <johnnypao43@gmail.com>
2018-12-04 17:14:55 -0700

    2018

--- Intex/Controllers/HomeController.cs
+++ Intex/Controllers/HomeController.cs
@@ -18,7 +18,18 @@ namespace Intex.Controllers
         [HttpPost]
         public ActionResult Login(string username, string password)
         {
-            return View();
+            if (username == "Seattle")
+            {
+                return RedirectToAction("Index", "Seattle");
+            }
+            else if (username == "Singapore")
+            {
+                return RedirectToAction("Index", "Singapore");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
